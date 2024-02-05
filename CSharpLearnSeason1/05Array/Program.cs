40396d8d5b4bd5b3d73ab58121901ce83ae9043b
Benjamin <bjzhou@outlook.com>
2024-02-05 20:39:19 +0800

    add 06Math...

---
+++ CSharpLearnSeason1/05Array/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _05Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Season1: 05Array\n");
+
+                int[] nums = { 2, 3, 5, 7, 11, 13, 17, 19 };
+
+                #region 遍历
+                {
+                    // fori遍历
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        Console.Write($"{nums[i]}, ");
+                    }
+
+                    Console.WriteLine();
+
+                    // foreach遍历
+                    foreach (var item in nums)
+                    {
+                        Console.Write("{0}, ", item);
+                    }
+                }
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpLearnSeason1/06Math/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _06Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Season1: 06Math\n");
+
+                #region N次方 / 开N次方根
+                {
+                    Console.WriteLine($"2^10 = {Math.Pow(2, 10)}");
+                    Console.WriteLine($"10^2 = {Math.Pow(10, 2)}");
+
+                    Console.WriteLine("{0}的平方根等于{1}", 2, Math.Pow(2, 1 / 2.0));
+                    Console.WriteLine("{0}的平方根等于{1}", 3, Math.Pow(3, 1 / 2.0));
+                    Console.WriteLine("{0}的平方根等于{1}", 5, Math.Pow(5, 1 / 2.0));
+                }
+                #endregion
+
+                Console.WriteLine("-----------------------------");
+
+                #region 对数
+                {
+                    Console.WriteLine($"log2 8 = {Math.Log2(8)}");
+                    Console.WriteLine($"lg10000 = {Math.Log10(9999)}");
+                    Console.WriteLine($"lne^4 = {Math.Log(Math.Pow(Math.E, 4), Math.E)}");
+                }
+                #endregion
+
+                Console.WriteLine("-----------------------------");
+
+                #region 三角函数(弧度)
+                {
+                    Console.WriteLine("sin{0} = {1}", "(4π/9)", Math.Sin(4 * Math.PI / 9));
+                    Console.WriteLine("cos{0} = {1}", "(4π/9)", Math.Cos(4 * Math.PI / 9));
+                    Console.WriteLine("tan{0} = {1}", "(4π/9)", Math.Tan(4 * Math.PI / 9));
+                }
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
