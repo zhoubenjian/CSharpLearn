00d5eeaf2227f62a207cbfb8d4b2c57e48474fa7
Benjamin <bjzhou@outlook.com>
2024-02-13 09:46:34 +0800

    05Array add something

--- CSharpLearnSeason1/05Array/Program.cs
+++ CSharpLearnSeason1/05Array/Program.cs
@@ -20,11 +20,53 @@
 
                     Console.WriteLine();
 
-                    // foreach遍历
+                    // foreach遍历(禁止修改值)
                     foreach (var item in nums)
                     {
                         Console.Write("{0}, ", item);
                     }
+                }
+                #endregion
+
+                Console.WriteLine($"\n---------------");
+
+                #region 最小值
+                {
+                    string input = Console.ReadLine();
+                    string[] strArray = input.Split(" ");
+                    int[] intArray = new int[strArray.Length];
+                    for (global::System.Int32 i = 0; i < intArray.Length; i++)
+                        intArray[i] = Convert.ToInt32(strArray[i]);
+
+                    int minValue = intArray[0];
+                    for (global::System.Int32 i = 1; i < intArray.Length; i++)
+                        minValue = (intArray[i] < minValue) ? intArray[i] : minValue;
+
+                    Console.WriteLine($"最小值：" + minValue);
+                }
+                #endregion
+
+                #region 插入排序
+                {
+                    // 输入有序数组
+                    string input = Console.ReadLine();
+                    string[] strArray = input.Split(" ");
+                    int[] intArray = new int[strArray.Length];
+                    for (global::System.Int32 i = 0; i < intArray.Length; i++)
+                        intArray[i] = Convert.ToInt32(strArray[i]);
+
+                    Console.WriteLine($"输入一个数：");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    int p = 0;
+                    for (global::System.Int32 i = 0; i < intArray.Length - 1; i++)
+                    {
+                        if (n >= intArray[i] && n <= intArray[i + 1])
+                        {
+                            p = i;
+                            break;
+                        }
+                    }
+                    Console.WriteLine($"{n}插入的位置是{p}");
                 }
                 #endregion
             }
