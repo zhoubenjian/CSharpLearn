b88103a67edbd7426743737e1d3e5c5c4601f2d3
Benjamin <bjzhou@outlook.com>
2024-02-17 20:08:21 +0800

    05Array add something...

--- CSharpLearnSeason1/05Array/Program.cs
+++ CSharpLearnSeason1/05Array/Program.cs
@@ -46,50 +46,141 @@
                 }
                 #endregion
 
+                Console.WriteLine($"\n---------------");
+
                 #region 插入排序
+                //{
+                //    // 输入有序数组
+                //    string input = Console.ReadLine();
+                //    string[] strArray = input.Split(" ");
+                //    int[] intArray = new int[strArray.Length];
+                //    for (global::System.Int32 i = 0; i < intArray.Length; i++)
+                //        intArray[i] = Convert.ToInt32(strArray[i]);
+
+                //    Console.WriteLine($"输入一个数：");
+                //    int n = Convert.ToInt32(Console.ReadLine());
+                //    int p = 0;
+                //    bool exist = false;
+                //    for (global::System.Int32 i = 0; i < intArray.Length - 1; i++)
+                //    {
+                //        if (intArray[0] > n)
+                //        {
+                //            p = -1;
+                //            exist = true;
+                //            break;
+                //        }
+                //        if (n >= intArray[i] && n <= intArray[i + 1])
+                //        {
+                //            p = i;
+                //            exist = true;
+                //            break;
+                //        }
+                //    }
+
+                //    if (exist == false)
+                //        p = intArray.Length;
+
+                //    int[] copyArray = new int[intArray.Length + 1];
+                //    for (global::System.Int32 i = 0; i < p + 1; i++)
+                //        copyArray[i] = intArray[i];
+
+                //    copyArray[p + 1] = n;
+
+                //    for (global::System.Int32 i = p + 1; i < intArray.Length; i++)
+                //        copyArray[i + 1] = intArray[i];
+                //    Console.WriteLine($"{n}插入的位置是{p}");
+
+                //    foreach (var item in copyArray)
+                //        Console.Write($"{item} ");
+                //}
+                #endregion
+
+                Console.WriteLine($"\n---------------");
+
+                #region 排序(Array.Sort)
                 {
-                    // 输入有序数组
+                    int[] intArray = new int[] { 9, 11, 4, 10, 8, 1 };
+                    // 系统方法排序
+                    Array.Sort(intArray);
+                    foreach (var item in intArray)
+                        Console.Write(item + " ");
+                }
+                #endregion
+
+                Console.WriteLine($"\n---------------");
+
+                #region 冒泡排序
+                {
+                    int[] intArray = new int[] { 9, 11, 4, 8, 22, 10 };
+                    for (int i = 0; i < intArray.Length - 1; i++)
+                    {
+                        // 是否发生交换
+                        bool isChange = false;
+                        for (global::System.Int32 j = 0; j < intArray.Length - i - 1; j++)
+                        {
+                            if (intArray[j] > intArray[j + 1])
+                            {
+                                int temp = intArray[j];
+                                intArray[j] = intArray[j + 1];
+                                intArray[j + 1] = temp;
+
+                                isChange = true;
+                            }
+                        }
+
+                        if (!isChange)
+                            break;
+                    }
+
+                    foreach (var item in intArray)
+                        Console.Write($"{item} ");
+                }
+                #endregion
+
+                Console.WriteLine($"\n---------------");
+
+                #region 不是回文串
+                {
+                    string inputString = Console.ReadLine();
+                    bool isHui = true;
+                    for (global::System.Int32 i = 0; i < inputString.Length / 2; i++)
+                    {
+                        if (inputString[i] != inputString[inputString.Length - 1 - i])
+                        {
+                            isHui = false; 
+                            break;
+                        }
+                    }
+                    Console.WriteLine(isHui);
+                }
+                #endregion
+
+                Console.WriteLine($"\n---------------");
+
+                #region 最多多少天气温升高
+                {
                     string input = Console.ReadLine();
                     string[] strArray = input.Split(" ");
                     int[] intArray = new int[strArray.Length];
                     for (global::System.Int32 i = 0; i < intArray.Length; i++)
                         intArray[i] = Convert.ToInt32(strArray[i]);
 
-                    Console.WriteLine($"输入一个数：");
-                    int n = Convert.ToInt32(Console.ReadLine());
-                    int p = 0;
-                    bool exist = false;
+                    int maxCount = 1;
+                    int count = 1;
                     for (global::System.Int32 i = 0; i < intArray.Length - 1; i++)
                     {
-                        if (intArray[0] > n)
+                        if (intArray[i + 1] > intArray[i])
                         {
-                            p = -1;
-                            exist = true;
-                            break;
+                            count++;
                         }
-                        if (n >= intArray[i] && n <= intArray[i + 1])
+                        else
                         {
-                            p = i;
-                            exist = true;
-                            break;
+                            if (count > maxCount)
+                                maxCount = count;
+                            count = 1;
                         }
                     }
-
-                    if (exist == false)
-                        p = intArray.Length;
-
-                    int[] copyArray = new int[intArray.Length + 1];
-                    for (global::System.Int32 i = 0; i < p + 1; i++)
-                        copyArray[i] = intArray[i];
-
-                    copyArray[p + 1] = n;
-
-                    for (global::System.Int32 i = p + 1; i < intArray.Length; i++)
-                        copyArray[i + 1] = intArray[i];
-                    Console.WriteLine($"{n}插入的位置是{p}");
-
-                    foreach (var item in copyArray)
-                        Console.Write($"{item} ");
+                    Console.WriteLine($"气温升高累计：{maxCount}天");
                 }
                 #endregion
             }
