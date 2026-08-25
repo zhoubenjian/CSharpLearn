a8ad90342fa01a99a35bd3b6167d539e3248308c
zhoubenjian <bjzhou@outlook.com>
2026-08-25 17:27:33 +0800

    implement 05OperatorOverload...

---
+++ CSharpLearnSeason2/05OperatorOverload/Box.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05OperatorOverload
+{
+    internal class Box
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+
+        /// <summary>
+        /// 体积计算
+        /// </summary>
+        /// <returns></returns>
+        public double Volume()
+        {
+            return Length * Width * Height;
+        }
+
+        /// <summary>
+        /// 运算符(+)重载
+        /// Overload the + operator to add two Box objects together
+        /// </summary>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static Box operator +(Box b, Box c)
+        {
+            return new Box(b.Length + c.Length, b.Width + c.Width, b.Height + c.Height);
+        }
+    }
+}
---
+++ CSharpLearnSeason2/05OperatorOverload/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _05OperatorOverload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- CSharpLearn2: 05OperatorOverload ---");
+
+
+            #region 运算符(+)重载
+            {
+                Box box1 = new Box(6.0, 7.0, 5.0);
+                Console.WriteLine($"box1的体积：{box1.Volume()}");
+
+                Box box2 = new Box(12.0, 13.0, 10.0);
+                Console.WriteLine($"box2的体积：{box2.Volume()}");
+
+
+                // 使用重载的运算符(+)将两个Box对象相加
+                Box box3 = box1 + box2;
+                Console.WriteLine("\nbox3的体积：{0}", box3.Volume());
+            }
+            #endregion
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
